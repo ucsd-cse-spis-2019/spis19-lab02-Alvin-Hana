3fa420cd0b3d9c5f6f4210925682cce03646fc44
Alvin Wang <spis19bm@its-cseb240-28.ucsd.edu>
2019-08-07 14:25:38 -0700

    getNumber function added

--- testSum.py
+++ testSum.py
@@ -1,7 +1,20 @@
 # The goal of this program is to practice Python constructs
+
+def getNumber():
+   symbols = input("Enter a digit: ")
+   combine = ""
+   symbols = int (symbols)
+   while symbols >=0:
+     combine += str (symbols)
+     symbols = input("Enter a digit: ")
+     symbols = int (symbols)
+   return combine
+
 def sumTwo(a,b):
    c = a + b
    return c
+
+print (getNumber())
 
 x = sumTwo(3,5)
 print(x)
