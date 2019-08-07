298902a8dcae3d4a9bd7377534ca1d561b913fe6
Alvin Wang <spis19bm@its-cseb240-28.ucsd.edu>
2019-08-07 14:48:41 -0700

    modified wageCalculator function that takes in the male wage and Korea as parameters

--- wageCalculator.py
+++ wageCalculator.py
@@ -1,8 +1,12 @@
 # wageCalculator.py
-def convertWageMtoW(mWage):
-   wageGap = 0.182
+# Takes in parameters for the male wage and a boolean for if the person is in Korea or not. Also hi Jaz!
+def convertWageMtoW(mWage, korea):
+   if korea:
+      wageGap = 0.346
+   else:
+      wageGap = 0.182
    ratio = 1-wageGap
    return mWage*ratio
-print (convertWageMtoW(100))
-print (convertWageMtoW(76.2))
-print (convertWageMtoW(0))
+print (convertWageMtoW(100, True))
+print (convertWageMtoW(76.2, False))
+print (convertWageMtoW(0, True))
